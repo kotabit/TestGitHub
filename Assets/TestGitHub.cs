af7e156ce960b980df128ee63cefb9ad6bb949b6
kotabit <102679860+kotabit@users.noreply.github.com>
2022-04-01 19:51:09 +0900

    コード変更

--- Assets/TestGitHub.cs
+++ Assets/TestGitHub.cs
@@ -7,10 +7,11 @@ public class TestGitHub : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
+    //aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
     void Update()
     {
         if(Input.GetKey(KeyCode.A))
